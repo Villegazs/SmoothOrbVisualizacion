e4e0a167675790add6b357566a2ae30504eca1f7
Villegazs <133931476+Villegazs@users.noreply.github.com>
2025-05-14 17:29:31 -0500

    Descripcion ciclica implementada

--- Assets/Scripts/OrbeDisplay.cs
+++ Assets/Scripts/OrbeDisplay.cs
@@ -4,22 +4,54 @@ using System;
 
 public class OrbeDisplay : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI tittleText;
+    [SerializeField] TextMeshProUGUI titleText;
     [SerializeField] TextMeshProUGUI descText;
     private DetectMouse actualOrbPart;
+    OrbeInfo currentOrbeInfo;
+    private int currentDescIndex;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public void ShowOrbeInfo(OrbeInfo info, DetectMouse actualPart)
     {
-        tittleText.text = info.nombre;
-        descText.text = info.descripcion;
+        currentOrbeInfo = info;
+        currentDescIndex = 0;
+        titleText.text = currentOrbeInfo.nombre;
+        descText.text = currentOrbeInfo.descripcionList[currentDescIndex];
         actualOrbPart = actualPart;
     }
 
     public void HideOrbeInfo()
     {
-        tittleText.text = string.Empty;
+        titleText.text = string.Empty;
         descText.text = string.Empty;
         actualOrbPart.EndExamination();
+    }
+
+    public void NextDescription()
+    {
+        if (currentOrbeInfo == null) return;
+
+        currentDescIndex++;
+        if (currentDescIndex >= currentOrbeInfo.descripcionList.Count)
+        {
+            currentDescIndex = 0; // Vuelve al inicio
+        }
+
+        UpdateDisplay();
+    }
+    private void UpdateDisplay()
+    {
+        if (currentOrbeInfo != null && currentDescIndex >= 0 && currentDescIndex < currentOrbeInfo.descripcionList.Count)
+        {
+            titleText.text = currentOrbeInfo.nombre;
+            descText.text = currentOrbeInfo.descripcionList[currentDescIndex];
+        }
+
+        UpdateNavigationButtons();
+    }
+
+    private void UpdateNavigationButtons()
+    {
+
     }
 }
--- Assets/Scripts/OrbeInfo.cs
+++ Assets/Scripts/OrbeInfo.cs
@@ -1,11 +1,11 @@
-using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "NewOrbeInfo", menuName = "Orbe/Orbe Info")]
 public class OrbeInfo : ScriptableObject
 {
     public string nombre;
-    [TextArea(3, 10)]
-    public string descripcion;
+    [TextArea(1,3)]
+    public List <string> descripcionList = new List<string>();
 
 }
